bb4b99c1b5ff0ef4b63c017cb1956320fa8a87a4
Federico G <federicogasior@hotmail.com>
2024-10-21 14:07:42 -0300

    Se agregan soluciones de listas

---
+++ 12 - Listas.pl
@@ -0,0 +1,53 @@
+% Suma de todos los valores de una lista
+
+suma([], 0).
+suma([Cabeza | Cola], Total):-
+    suma(Cola, Acum),
+    Total is Acum + Cabeza.
+
+
+
+% Contar cuantos valores repetidos de un elemento hay en una lista.
+
+% Se podría haber usado ; en vez de definir otra regla
+igual_int(A, A, 1).
+igual_int(A, B, 0):-A \== B.
+
+contar([], _, 0).
+contar([Cabeza | Cola], X, Total):-
+    contar(Cola, X, Parcial),
+    igual_int(Cabeza, X, A),
+    Total is Parcial + A.
+
+
+
+% Buscar todas las posiciones de un elemento en una lista
+
+% Si X es el primer elemento de la sublista, Pos es 0
+indice_de([X | _], X, 0).
+
+% No importa que relacion tenga cabeza con X, sigo consultando el resto de los elementos
+% Para cada elemento que haya sido matcheada por la regla anterior, voy a incrementar su posicion hasta la actual
+indice_de([_ | Cola], X, Posicion):-
+    indice_de(Cola, X, Posicion1),
+    Posicion is Posicion1 + 1.
+
+
+
+% Para un listado de 0 a n hechos nota/1, con notas de 1 a 10, hallar el promedio de las mismas
+
+nota(2).
+nota(6).
+nota(2).
+nota(2).
+nota(9).
+nota(7).
+
+promedio_nota(Promedio):-
+    findall(X, (nota(X)), ListaNotas),
+    ListaNotas \= [], % Previene dividir por 0
+    sum_list(ListaNotas, Suma),
+    length(ListaNotas, Largo),
+    Promedio is Suma / Largo.
+
+
